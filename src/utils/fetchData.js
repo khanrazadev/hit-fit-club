b3db1aa3e2c1066a3265d25b863dff5131dbd430
raza <razakhanha05@gmail.com>
2023-08-29 00:48:51 +0530

    update-api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ba78f1ac2amsh5370676efc2046cp12994djsn6db15cc3dbc3',
+      'X-RapidAPI-Key': '10d6236308msh1b873d82c44a2eep19add3jsn8ea5475da1c0',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
    
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '0727101e27msh6b6cfaa09659808p1255f8jsn079f85ef8154',
+    'X-RapidAPI-Key': '10d6236308msh1b873d82c44a2eep19add3jsn8ea5475da1c0',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
