4cb73744ba8bda5a3f1dfa03c66766cfd03d46b2
raza <razakhanha05@gmail.com>
2023-08-29 05:51:45 +0530

    emv

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+import React from "react";
+import { Stack } from "@mui/material";
 
 const Loader = () => (
-  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <InfinitySpin color="grey" />
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
+    <Loader color="grey" />
   </Stack>
 );
 
