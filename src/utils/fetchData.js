19bd6f7f789e4acd50145a18f2e61efce6fa9d7a
raza <razakhanha05@gmail.com>
2023-09-06 19:17:12 +0530

    Fixed updated api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "10d6236308msh1b873d82c44a2eep19add3jsn8ea5475da1c0",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "10d6236308msh1b873d82c44a2eep19add3jsn8ea5475da1c0",
+    "X-RapidAPI-Key": process.env.REACT_APP_YT_KEY,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
